79cdb730494e2979b59c16aff26186325a0a35fb
Noah Pivnick <nopivnick@nopivnick.com>
2021-10-27 16:47:14 -0400

    Create install-node-v12.22.7.sh

---
+++ install-node-v12.22.7.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export NODE_VER=12.22.7
+if ! node --version | grep -q ${NODE_VER}; then
+  (cat /proc/cpuinfo | grep -q "Pi Zero") && if [ ! -d node-v${NODE_VER}-linux-armv6l ]; then
+    echo "Installing nodejs ${NODE_VER} for armv6 from unofficial builds..."
+    curl -O https://unofficial-builds.nodejs.org/download/release/v${NODE_VER}/node-v${NODE_VER}-linux-armv6l.tar.xz
+    tar -xf node-v${NODE_VER}-linux-armv6l.tar.xz
+  fi
+  echo "Adding node to the PATH"
+  PATH=$(pwd)/node-v${NODE_VER}-linux-armv6l/bin:${PATH}
+fi
+node --version # or whatever ....
